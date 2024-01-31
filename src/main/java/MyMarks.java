6a1e1eae39a1f65241e0511ed2cc3349c6d8ba68
subhashree-17 <cvsubhashree17@gmail.com>
2024-01-31 12:04:44 +0530

    initial commit

---
+++ src/main/java/MyMarks.java
@@ -0,0 +1,11 @@
+public class MyMarks {
+    public static void main(String[] args){
+        System.out.println("my marks");
+        System.out.println("first sem GPA:8.8");
+        System.out.println("second sem GPA:8.9");
+        System.out.println("third sem GPA:8.7");
+        System.out.println("fourth sem GPA:8.9");
+        System.out.println("fifth sem GPA:8.1");
+        System.out.println("sixth sem GPA:8.3");
+    }
+}
---
+++ src/main/java/org/example/PersonalInfo.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class PersonalInfo {
+
+    public static void main(String[] args){
+        System.out.println("my personal information");
+        System.out.println("name:subhashree venkatraman");
+        System.out.println("age:21");
+        System.out.println("email:cvsubhashree17@gmail.com");
+        System.out.println("department:biotechnology");
+        System.out.println("college:VISTAS");
+        System.out.println("contact:8608992461");
+    }
+}

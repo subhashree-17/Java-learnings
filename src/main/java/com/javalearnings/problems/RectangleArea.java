912bcd7194e6e1f2cdd71716274f9e5a18dd8300
subhashree-17 <cvsubhashree17@gmail.com>
2024-03-08 20:00:47 +0530

    Example

---
+++ src/main/java/com/javalearnings/problems/RectangleArea.java
@@ -0,0 +1,21 @@
+package com.javalearnings.problems;
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Rectangle Area Calculator");
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+
+        scanner.close();
+    }
+
+}
